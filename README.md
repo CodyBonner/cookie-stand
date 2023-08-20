f069f83918d02674dd9e7a32bf55f0a68b882954
Cody Bonner <bonner_cody94@yahoo.com>
2023-08-20 20:22:50 +0000

    figured out random customers and their average cookies

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # cookie-stand
 repo that will host cookie-stand involving sales, future ordering, based on DOM principles.
+
+
+# LAB - 5
+
+## About Me
+
+To utilize the skills and tools I have achieved to showcase a unique and personal webpage dedicated to letting others know just a bit more about me.
+
+### Author: Cody Bonner
+
+### Links and Resources
+
+* [GitHub Class Repo](https://github.com/CodyBonner/code-challenges)
+
+
+* 
+
+### Lighthouse Accessibility Report Score
+
+![LightHouse-Result](img/lightHouse_score.JPG)
+
+### Reflections and Comments
+
+Learning Journal 3:
+
+Paper View
+This past week has been beneficial to me on a learning experience because not only did I get to continue putting in time learning the fundamentals of the languages I will be using for software development. I also learned the importance in my own practice to fully utilize IDs when it comes to  the relationship between HTML and CSS.
+
+I also learned that while I may have a good deal of knowledge when it comes to computer languages that are used in software development. There's still plenty I haven't learned but that go hand in hand with many ideas I have when it comes to website develop and just how much goes into simple things like displaying an image on alert based on a user response. It's not as simple as I thought which isn't a bad thing. It gives me something exciting to look forward to.
+
+I look forward to learning more and developing my skills further.
---
+++ js/app.js
@@ -0,0 +1,115 @@
+'use strict';
+
+//object literal for seattle store
+
+const seattle = {
+  name: 'Seattle',
+  customersMin: 23,
+  customersMax: 65, 
+  hours: [14],
+  avgCookiesPerCustomer: 6.3,
+  cstPHr: [], 
+  cookiesPHr: [],
+  test: function(){
+    let custArray = [];
+    for(let i =0; i < this.hours; i++){
+      const ranCustNum = Math.floor(Math.random()*(this.customersMax-this.customersMin)+this.customersMin);
+      custArray.push(ranCustNum);
+    }
+    this.cstPHr = custArray;
+    return this.cstPHr;
+  }, 
+
+  //section below takes the random number of customers per hour and multiplies it by the
+  //average cookies sold per customer for example 2 customers per hour time 6.3 cookies is 12.6 rounded
+  //down to 12 for a whole number.
+  cookies: function(){
+    const cookiesArray = this.cstPHr;
+    const cookieMultiplier = this.avgCookiesPerCustomer;
+    const multipliedArray = [];
+    for(const num of cookiesArray){
+      const multipliedValue = parseInt(num * cookieMultiplier);
+      multipliedArray.push(multipliedValue);
+    }
+    console.log(multipliedArray);
+    return(multipliedArray);
+    //console.log(cookiesArray);
+
+
+  },
+
+  render: function() {
+    const seattleContainer = document.getElementById('test');
+
+    const seattleName = document.createElement('p');
+    seattleName.textContent = `City: ${this.name}`;
+
+    seattleContainer.appendChild(seattleName);
+
+    const customerNumber = document.createElement('p');
+    customerNumber.textContent = `Random number of customers per hour: ${this.test()}`;
+    seattleContainer.appendChild(customerNumber);
+
+    const cookieNumber = document.createElement('li');
+    cookieNumber.textContent = `Random cookies per hour: ${this.cookies()}`;
+    seattleContainer.appendChild(cookieNumber);
+
+
+  }
+};
+
+seattle.render();
+
+
+
+//console.log(parseInt(seattle.cstPHr) * seattle.avgCookiesPerCustomer);
+
+
+
+
+
+/*
+function getDataForCity(city){
+  let randomCustomerNumber = city.customersPerHour();
+  let cityMinCust = city.customersMin / randomCustomerNumber;
+  let resultArray = [];
+  resultArray.push(cityMinCust);
+  return resultArray;
+}
+
+
+
+
+
+//cookies per hour
+let seattleHourlyCookies = getHourlyCookierForCity(seattle);
+
+function getHourlyCookierForCity(city){
+  let result = [];
+  for(let i = 0; i < 14; i++){
+    let randomCustomerNumber = city.customersPerHour();
+    let hourPurchasedCookies = randomCustomerNumber * city.avgCookiesPerCustomer;
+    result.push(hourPurchasedCookies);
+  }
+  return result;
+}
+
+console.log(seattleHourlyCookies);
+
+let randomCustomerNumber = seattle.customersPerHour();
+console.log(randomCustomerNumber);
+let seattleMinCust =  seattle.customersMin / randomCustomerNumber;
+
+let seattleData;
+
+console.log(seattleMinCust);
+
+
+
+
+seattleData = getDataForCity(seattle);
+
+console.log(seattleData);
+
+
+*/
